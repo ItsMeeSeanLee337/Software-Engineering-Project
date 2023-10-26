6ffd0a1c0f2d007f0bca8318b1ead81749384ec5
Sean Lee <slee206061@gmail.com>
2023-10-26 16:40:17 -0400

    updated

--- react-app/src/Pages/nutritional_info.js
+++ react-app/src/Pages/nutritional_info.js
@@ -1,99 +1,61 @@
-'../styles/Nutritionalinfo.css'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import axios from 'axios';
 
+function NutritionalInformation() {
+  const [foodItem, setFoodItem] = useState('');
+  const [nutritionalInfo, setNutritionalInfo] = useState(null);
 
-function NutritionalInfo() {
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    // Replace 'YOUR_SPOONACULAR_API_KEY' with your actual API key
+    const apiKey = 'YOUR_SPOONACULAR_API_KEY';
+
+    try {
+      const response = await axios.get(
+        `https://api.spoonacular.com/food/ingredients/search?query=${foodItem}&apiKey=${apiKey}`
+      );
+
+      if (response.data.results && response.data.results.length > 0) {
+        // Assuming the first result contains the desired information
+        const firstResult = response.data.results[0];
+        setNutritionalInfo(firstResult);
+      } else {
+        // Handle no results found
+        setNutritionalInfo(null);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div>
-      <header>
-        <h1>Nutritional Information</h1>
-        <h2>Banana</h2>
-      </header>
+      <Navbar />
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Enter a food item"
+          value={foodItem}
+          onChange={(e) => setFoodItem(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
 
-      <div className="nutritional-info">
-        <img src="banana.jpg" alt="Banana" id="banana-image" />
-        <h2>Nutritional Content per 100g:</h2>
-        <p>
-          <span className="nutrient-label">Calories:</span>
-          <span className="nutrient-value">89</span> kcal
-        </p>
-        <p>
-          <span className="nutrient-label">Fats:</span>
-          <span className="nutrient-value">0.3g</span>
-        </p>
-        <p>
-          <span className="nutrient-label">Saturated Fats:</span>
-          <span className="nutrient-value">0.1g</span>
-        </p>
-        <p>
-          <span className="nutrient-label">Sugars:</span>
-          <span className="nutrient-value">12g</span>
-        </p>
-        <p>
-          <span className="nutrient-label">Salts:</span>
-          <span className="nutrient-value">0.01g</span>
-        </p>
-      </div>
+      {nutritionalInfo ? (
+        <div>
+          <h2>Nutritional Information for {nutritionalInfo.name}</h2>
+          <ul>
+            <li>Calories: {nutritionalInfo.calories}</li>
+            {/* Add more nutritional information fields here */}
+          </ul>
+        </div>
+      ) : (
+        <p>No nutritional information found for the entered food item.</p>
+      )}
     </div>
   );
 }
 
-export default NutritionalInfo;
-
-//OR:
-const bananaData = {
-    name: 'Banana',
-    image: 'banana.jpg',
-    nutritionalInfo: {
-      Calories: '89 kcal',
-      Fats: '0.3g',
-      'Saturated Fats': '0.1g',
-      Sugars: '12g',
-      Salts: '0.01g',
-    },
-  };
-  
-  const appleData = {
-    name: 'Apple',
-    image: 'apple.jpg',
-    nutritionalInfo: {
-      Calories: '52 kcal',
-      Fats: '0.2g',
-      'Saturated Fats': '0.03g',
-      Sugars: '14g',
-      Salts: '0.002g',
-    };
-  };
-  
-  // Use the NutritionalInfo component with different food data
-  <NutritionalInfo foodData={bananaData} />;
-  <NutritionalInfo foodData={appleData} />;
-  
-
-function NutritionalInfo({ foodData }) {
-  const { name, image, nutritionalInfo } = foodData;
-
-  return (
-    <div>
-      <header>
-        <h1>Nutritional Information</h1>
-        <h2>{name}</h2>
-      </header>
-
-      <div className="nutritional-info">
-        <img src={image} alt={name} id="food-image" />
-        <h2>Nutritional Content per 100g:</h2>
-        {Object.keys(nutritionalInfo).map((nutrient) => (
-          <p key={nutrient}>
-            <span className="nutrient-label">{nutrient}:</span>
-            <span className="nutrient-value">{nutritionalInfo[nutrient]}</span>
-          </p>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default NutritionalInfo;
-
-
+export default NutritionalInformation;
